ef603f902e18d49d27b5f389dd7190cf5db79c15
ameerlouly <loulyameer@gmail.com>
2025-06-04 22:09:52 +0200

    Go back N, first test

--- src/main.c
+++ src/main.c
@@ -124,7 +124,7 @@ typedef struct {
 #define T1 					( pdMS_TO_TICKS(100) )
 #define T2 					( pdMS_TO_TICKS(200) )
 #define Tout 				( pdMS_TO_TICKS(225) )	// 150, 175, 200, 225
-#define Pdrop 				( (double)0.08 )		// 0.01, 0.02, 0.04, 0.08
+#define Pdrop 				( (double)0.01 )		// 0.01, 0.02, 0.04, 0.08
 #define P_ack 				( (double)0.01 )
 #define P_WRONG_PACKET		( (double)0.0 )
 #define Tdelay				( pdMS_TO_TICKS(50) )
@@ -373,7 +373,7 @@ int main(int argc, char* argv[])
 	{
 		// Creating Tasks
 		status = xTaskCreate(vSenderTask, "Node 1", 512, (void*)&Node1, 1, &Node1Task);
-		// status &= xTaskCreate(vSenderTask, "Node 2", 512, (void*)&Node2, 1, &Node2Task);
+		status &= xTaskCreate(vSenderTask, "Node 2", 512, (void*)&Node2, 1, &Node2Task);
 		status &= xTaskCreate(vRecieverTask, "Node 3", 512, (void*)&Node3, 2, &Node3Task);
 		status &= xTaskCreate(vRecieverTask, "Node 4", 512, (void*)&Node4, 2, &Node4Task);
 		status &= xTaskCreate(vRouterTask, "Router", 512, (void*)&Router, 3, &RouterTask);
